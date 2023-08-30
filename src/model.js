9213c29f1b4c521d542240616fba5acbe18d90e2
Priscila Schleske <plasch7@gmail.com>
2023-08-30 15:43:02 -0600

    finishes the movie model

--- src/model.js
+++ src/model.js
@@ -32,3 +32,36 @@ User.init(
     sequelize: db,
   },
 );
+
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Movie.init(
+  {
+    movieId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    overview: {
+      type: DataTypes.STRING
+    },
+    releaseDate: {
+      type: DataTypes.DATE
+    },
+    posterPath: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    modelName: 'movie',
+    sequelize: db,
+  }
+)
