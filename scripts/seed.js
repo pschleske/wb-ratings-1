d386e5f0068933a2d322ac1526468b9aa2e3805b
Priscila Schleske <plasch7@gmail.com>
2023-08-30 19:08:16 -0600

    adds test users to db

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,6 @@
 import { Movie, Rating, User, db } from '../src/model.js';
 import movieData from './data/movies.json' assert {type: 'json'};
+import userData from './data/users.json' assert {type: 'json'};
 
 console.log('Syncing database...');
 
@@ -19,5 +20,17 @@ const moviesInDB = movieData.map(async (movie) => {
 })
 
 await Promise.all(moviesInDB)
+// console.log(moviesInDB)
 
-db.close()
+const usersInDB = userData.map(async (user) => {
+    return await User.create({
+        email: user.email,
+        password: user.password
+    })
+});
+
+await Promise.all(usersInDB)
+
+// console.log(usersInDB)
+
+await db.close()
