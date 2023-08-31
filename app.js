bcb5f702387809f22c40106760f4577cc2e98059
Priscila Schleske <plasch7@gmail.com>
2023-08-30 21:30:44 -0600

    adds app.get routes and app.post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import morgan from 'morgan';
 import ViteExpress from 'vite-express';
+import { Movie, Rating, User } from './src/model.js';
 
 const app = express();
 const port = '8000';
@@ -11,5 +12,34 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+
+// ROUTES HERE:
+app.get('/movies', async (req, res) => {
+    const allMovies = await Movie.findAll();
+    res.json(allMovies);
+});
+
+app.get('/movies/:movieId', async (req, res) => {
+    const { movieId } = req.params;
+    const movie = await Movie.findByPk(movieId);
+    res.json(movie)
+})
+
+app.post('/auth', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+        where: { email: email }
+    });
+
+    if (user && user.password === password) {
+        req.session.userId = user.userId;
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+
+
 
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
